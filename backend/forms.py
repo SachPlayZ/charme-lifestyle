bdeb514d68d0afc9c0c0f03a7ad40558828b5aa5
pradyumna-bhowmick <pradyumna.bhowmick.2016@gmail.com>
2024-02-06 15:28:54 +0530

    updated backend

--- backend/forms.py
+++ backend/forms.py
@@ -1,13 +1,16 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, EmailField
 from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    username = StringField('Username', validators=[DataRequired()], render_kw={"placeholder": "Username"})
+    password = PasswordField('Password', validators=[DataRequired()], render_kw={"placeholder": "Password"})
     submit = SubmitField('Login')
 
 class SignupForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    name = StringField(label='Name', validators=[DataRequired()], render_kw={"placeholder": "Your Name"})
+    username = StringField('Username', validators=[DataRequired()], render_kw={"placeholder": "Username"})    
+    email = EmailField("Email Address", [DataRequired(), "Please enter a valid email address"])
+    password = PasswordField('Password', validators=[DataRequired()], render_kw={"placeholder": "Password"})
+    confirm_password = PasswordField('Confirm Password', [DataRequired(password), 'Passwords must match'], render_kw={"placeholder": "Confirm Password"})
     submit = SubmitField('Signup')
