be81c4501ece559ccef754190d3268323dc34730
Arnab S <arnabsengupta104@gmail.com>
2024-02-08 05:46:52 +0530

    Added cool desings, fixed backend, did awesome job #coder #supercoder #superarnab

--- frontend/src/components/LandingPage.js
+++ frontend/src/components/LandingPage.js
@@ -84,23 +84,24 @@ const LandingPage = () => {
                 </nav>
             </header>
             <div id="page" className="">
-            <section id="vision" style={{ 
-    backgroundImage: "url(https://cdn.discordapp.com/attachments/1170293654896787498/1204389803517542480/Vision.jpg?ex=65d48e69&is=65c21969&hm=4ca24da7accd6e997030fb0dfc85048ae20c614806518e5a24c8464aca859681&)",
-    backgroundRepeat: "no-repeat",
-    backgroundSize: "cover"
-}}>
-    <h1>Charm</h1><h1 className='pink'>è</h1><h1>.</h1>
-</section>
+                <section id="vision" style={{
+                    backgroundImage: "url(https://cdn.discordapp.com/attachments/1170293654896787498/1204389803517542480/Vision.jpg?ex=65d48e69&is=65c21969&hm=4ca24da7accd6e997030fb0dfc85048ae20c614806518e5a24c8464aca859681&)",
+                    backgroundRepeat: "no-repeat",
+                    backgroundSize: "cover"
+                }}>
+                    <h1>Charm</h1><h1 className='pink'>è</h1><h1>.</h1>
+                </section>
 
-<section id="knowledge" style={{ 
-    backgroundImage: "url(https://cdn.discordapp.com/attachments/1169346758636224614/1204509432067792916/image.png?ex=65d4fdd3&is=65c288d3&hm=fa6be7d5bb223e8d3bef5fd6cd8cdbed9d52d1cc41341e92ff1be086e8d970eb&)",
-    backgroundRepeat: "no-repeat",
-    backgroundSize: "cover"
-}}>
-    <h2>Generate your Style.</h2>
-    <Link to='AiStylist' class="button-52" role="button">AI Stylist</Link>
+                <section id="knowledge" style={{
+                    backgroundImage: "url(https://cdn.discordapp.com/attachments/1169346758636224614/1204509432067792916/image.png?ex=65d4fdd3&is=65c288d3&hm=fa6be7d5bb223e8d3bef5fd6cd8cdbed9d52d1cc41341e92ff1be086e8d970eb&)",
+                    backgroundRepeat: "no-repeat",
+                    backgroundSize: "cover"
+                }}>
+                    <h2>Find your style.</h2>
+                    <br/>
+                    <Link to='AiStylist' class="button-52" role="button">AI Stylist</Link>
 
-</section>
+                </section>
 
                 <section id="space">
                     <h2>Marketplace</h2>
@@ -126,24 +127,81 @@ const LandingPage = () => {
                                 <p>₹ 120</p>
                             </div>
                         </div>
-                        
+
                     </div>
                     <Link to='/marketplace' class="button-57" role="button"><span class="text">See More</span><span>Let's Go!</span></Link>
-                    
+
                 </section>
-                <section id="future" style={{ 
-                    backgroundImage: "url(https://assets.codepen.io/214624/future.jpg)",
-                    backgroundRepeat: "no-repeat",
-                    backgroundSize: "cover"
-            }}>
-                    <h2>Contact Us</h2>
-                    <form className='contactform'>
-                        <input className='cont-text' type="text" placeholder="Name" />
-                        <input className="email-text" type="email" placeholder="Email" />
-                        <textarea className='message-text ' placeholder="Message"></textarea>
-                        <button>Send</button>
-                    </form>
-                </section>
+                <div id="future" style={{
+                    // backgroundImage: "url(https://assets.codepen.io/214624/future.jpg)",
+                    // backgroundRepeat: "no-repeat",
+                    // backgroundSize: "cover"
+                    backgroundColor: "black",
+                }}>
+                    <div className="footer">
+                        <div className="bubbles">
+                            {[...Array(128)].map((_, i) => (
+                                <div
+                                    key={i}
+                                    className="bubble"
+                                    style={{
+                                        '--size': `${9 + Math.random() * 4}rem`,
+                                        '--distance': `${8 + Math.random() * 4}rem`,
+                                        '--position': `${-3 + Math.random() * 110}%`,
+                                        '--time': `${3 + Math.random() * 2}s`,
+                                        '--delay': `${-1 * (2 + Math.random() * 2)}s`,
+                                    }}
+                                />
+                            ))}
+                        </div>
+                        <div className="content">
+                            <div style={{
+                                color: "black",
+                            }}>
+                                <h1><b>Charmè Lifestyle</b></h1>
+                                <h3><b>Contact Us</b></h3>
+                                charmelifestyleindia@gmail.com<br />
+                                (+91) 933 086 5517<br />
+                                <a href="https://instagram.com/username" target="_blank">
+                                    <svg
+                                        version="1.1"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        xmlnsXlink="http://www.w3.org/1999/xlink"
+                                        x="0px"
+                                        y="0px"
+                                        width="30px"
+                                        height="30px"
+                                        viewBox="0 0 30 30"
+                                        enableBackground="new 0 0 30 30"
+                                        xmlSpace="preserve"
+                                    >
+                                        <path
+                                            id="instagram"
+                                            fill="#ffffff"
+                                            d="M22.107,3.415H7.893c-2.469,0-4.479,2.007-4.479,4.477v4.73v9.486c0,2.469,2.01,4.479,4.479,4.479h14.215
+	c2.469,0,4.479-2.01,4.479-4.479v-9.486v-4.73C26.586,5.421,24.576,3.415,22.107,3.415 M23.393,6.086l0.512-0.004v0.511v3.416
+	l-3.916,0.014l-0.012-3.928L23.393,6.086z M11.693,12.622c0.742-1.028,1.945-1.7,3.307-1.7s2.564,0.672,3.307,1.7
+	c0.484,0.67,0.771,1.49,0.771,2.379c0,2.248-1.828,4.078-4.078,4.078c-2.248,0-4.078-1.83-4.078-4.078
+	C10.922,14.112,11.211,13.292,11.693,12.622 M24.328,22.107c0,1.225-0.994,2.219-2.221,2.219H7.893
+	c-1.225,0-2.219-0.994-2.219-2.219v-9.486h3.459C8.832,13.356,8.664,14.159,8.664,15c0,3.494,2.842,6.335,6.336,6.335
+	s6.336-2.842,6.336-6.335c0-0.842-0.17-1.645-0.467-2.379h3.459V22.107z"
+                                        />
+                                    </svg>
+                                </a>
+                            </div>
+                            <div>
+                                <a
+                                    className="image"
+                                    href="https://www.instagram.com/eldew/"
+                                    style={{
+                                        backgroundImage: 'url("https://cdn.discordapp.com/attachments/1170293654896787498/1204471233727692850/image.png?ex=65d4da40&is=65c26540&hm=619cb1f1107bb16f8b45950a05753aeb8cb1e6d3c12190b5c9343133989cd5e5&")',
+                                    }}
+                                />
+                                <p>©2024 Charmè</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
--- frontend/src/components/Marketplace.js
+++ frontend/src/components/Marketplace.js
@@ -68,26 +68,6 @@ const Marketplace = () => {
                   </div>
                 </div>
               ))}
-            <div className="col pb-3 ">
-              <div className="card ms-auto me-auto">
-                <img
-                  src="https://cdn.discordapp.com/attachments/1170293654896787498/1204478466356019260/riddhi-fashion-men-shirt-casual-shirts-navy-blue02-xl-trendy-shirts-men-shirts-shirts-casual-shirt-cotton-shirts-shirt-men-shirt-product-images-rvnszwuiph-0-202211101117.webp?ex=65d4e0fc&is=65c26bfc&hm=de0a1fea382f62967daef04e54633e2a6c78cd32ab87d98fae7def498e6f55ba&"
-                  className="card-img-top sqrmage"
-                  alt=""
-                />
-                <div className="card-body">
-                  <h5 className="card-title">Shirt</h5>
-                  <p className="card-text">₹ 140</p>
-                  <Link class="button-1" role="button">
-                    Buy Now
-                  </Link>
-
-                  <Link class="button-81" role="button">
-                    Add to Cart
-                  </Link>
-                </div>
-              </div>
-            </div>
           </div>
         </div>
       </div>
